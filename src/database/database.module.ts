3b81201696eaf8aa8ac943a1c3e0399436c34e76
officefish <fusikshukovky@gmail.com>
2025-03-19 09:39:02 +0300

    remove data folder from git

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,31 +1,4 @@
-<<<<<<< HEAD
 
-import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-
-@Module({
-    imports: [
-      TypeOrmModule.forRootAsync({
-        imports: [ConfigModule],
-        inject: [ConfigService],
-        useFactory: (configService: ConfigService) => ({
-          type: 'postgres',
-          host: configService.get('POSTGRES_HOST') ?? 'localhost',
-          port: configService.get('POSTGRES_PORT'),
-          username: configService.get('POSTGRES_USER') ?? 'postgres',
-          password: configService.get('POSTGRES_PASSWORD') ?? 'postgres',
-          database: configService.get('POSTGRES_DB') ?? 'wanagodb',
-          entities: [
-            __dirname + '/../**/*.entity.ts',
-          ],
-          synchronize: true,
-        })
-      }),
-    ],
-  })
-  export class DatabaseModule {}
-=======
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -50,9 +23,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
         synchronize: true,
         retryDelay: 2000,  // Wait 2 seconds between retries
         retryAttempts: 4, // Increase retry attempts
+        logging: true
       })
     }),
   ],
 })
 export class DatabaseModule {}
->>>>>>> d231611eb0435dd02e32852d2a633e8ac8d05fac
